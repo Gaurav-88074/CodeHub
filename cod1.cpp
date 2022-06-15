e03f6a6d43be8cbedb5dbb5c01b0a9eec70f2eab
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2022-06-15 21:01:57 +0530

    first commit

---
+++ cod1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+bool isPalindrome(string s,int start,int end){
+    if(start>end){
+        return true;
+    }
+    else if(s[start]!=s[end]){
+        return false;
+    }
+    else{
+        return isPalindrome(s,start+1,end-1);
+    }
+    
+}
+int main(){
+    string s = "hello";
+
+    if (isPalindrome(s,0,s.length()-1)){
+        cout<<s<<" is palindrome"<<endl;
+    }
+    else{
+        cout<<s<<" isn't palindrome"<<endl;
+    }
+    
+    return 0;
+}
