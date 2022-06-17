aef7661024d94613a9d2749be8535571a59d5834
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2022-06-17 15:19:58 +0530

    code6.cpp

---
+++ code6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int> compute(vector<int> &array){
+    sort(array.begin(),array.end());
+    for(int i =1 ; i<=array.size() ;i++ ){
+        if (array[i-1]!=i){
+            return {array[i-1],i};
+        }
+    }
+    return {};
+}
+int main(){
+    
+    vector<int> array = {3,1,2,5,3};
+
+    vector<int> res = compute(array);
+
+    cout<<res[0]<<" "<<res[1]<<endl;
+
+    return 0;
+}
